f25a422ea18049f9d931ac5629794833c77496b4
lucascardozo <lucascardozo20034@gmail.com>
2022-08-29 22:59:12 -0300

    Commit 1

--- products.js
+++ products.js
@@ -1,49 +1,38 @@
-const carro = ('https://japceibal.github.io/emercado-api/cats_products/101.json');
-
-fetch(carro)
-.then((response) => {
 
 
+const URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 
 
-response.json()
-debugger
-})
+//funcion que cargará los datos obtenidos:
+fetch(URL)
+.then( respuesta => respuesta.json() )
+.then( data => {
+    const ArrayDatos = data;
+    showList(ArrayDatos.products);
+} )
 
+function showList(array){
+    array.forEach (element => {
+        var elementHTML = `  <div class="list-group-item list-group-item-action">
+                                <div class="row">
+                                    <div class="col-3">
+                                        <img src="${element.image}" alt="auto-gerico" class="img-thumbnail">  
+                                    </div>   
+                                    <div class="col">
+                                        <div class="d-flex w-100 justify-content-between">
+                                            <div>
+                                                <h4>${element.name}</h4>
+                                                <p>${element.description}</p>
+                                            </div>
+                                            <small>${element.soldCount}</small>
+                                        </div>
+                                    </div>
+                                </div>  
+                            </div> 
 
-
-//array donde se cargarán los datos recibidos:
-
-let categoriesArray = [];
-
-//función que recibe un array con los datos, y los muestra en pantalla a través el uso del DOM
-function showCategoriesList(array){
-    let htmlContentToAppend = "";
-
-    for(let i = 0; i < array.length; i++){ 
-        let category = array[i];
-        htmlContentToAppend +=  `  
-      <div class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="  category.image  " alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4> category.name </h4> 
-                        <p>  category.description </p> 
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        `
-        
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
-    }
+                            `
+         document.getElementById("info").innerHTML += elementHTML;
+    });
 }
 
-showCategoriesList(carro)
+showList()
