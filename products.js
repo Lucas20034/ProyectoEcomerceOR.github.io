1b12d38c41f1480d651a8f28d316a7e8950bb723
lucascardozo <lucascardozo20034@gmail.com>
2022-09-04 15:15:29 -0300

    GitPUSH

--- products.js
+++ products.js
@@ -1,39 +1,126 @@
-URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
+let PRODUCTS_API = PRODUCTS_URL + localStorage.getItem('catID') + '.json'
+let productList = ""
+let catNames = { 101: 'Autos',102: "Juguetes",103: "Muebles", 104: "Herramientas",105: "Computadoras",10: "Vstimenta",107:"Electrodomesticos",10:"Deporte", 109: "Celulares"
+ }     ;
 
 
-//peticion que cargará los datos obtenidos:// 
-fetch(PRODUCTS_URL + localStorage.getItem('catID') + EXT_TYPE) //Hago uso de constantes ya declaradas que tienen los elementos del fetch fetch que necesito , luego para llamar a la array de objetoss me traigo los item de la Local Storage//
+ 
 
-.then( respuesta => respuesta.json() )
-.then( data => {
-    const ArrayDatos = data;
-    showinfo(ArrayDatos.products);
-} )
 
-function showinfo(array){
-    array.forEach(element => {
-        var elementHTML =  `
-        
-        <div class="list-group-item list-group-item-action">
-                                <div class="row">
-                                    <div class="col-3">
-                                        <img src="${element.image}" alt="auto-gerico" class="img-thumbnail">  
-                                    </div>   
-                                    <div class="col">
-                                        <div class="d-flex w-100 justify-content-between">
-                                            <div>
-                                                <h4>${element.name} -  ${element.currency} ${element.cost} </h4>
-                                                <p>${element.description}</p>
-                                            </div>
-                                            <small>${element.soldCount}</small>
-                                        </div>
-                                    </div>
-                                </div>  
-                            </div> 
-                            ` 
-         document.getElementById("info").innerHTML += elementHTML;
-    });
+//Lo de mas arriba//
+
+
+
+
+
+
+
+
+
+//Lo del medio//
+
+//Funciones ASC-DESC//
+
+
+
+
+//DSC//
+
+function sortByPriceDesc(a,b){
+
+    return a.cost -  b.cost 
+
+}
+//ASC//
+
+function sortByPriceAsc(a,b){
+
+    return b.cost -  a.cost 
 
 }
 
 
+//More action//
+
+
+function productSortMore(){
+
+    showProducts(productList.sort(sortByPriceDesc));
+
+
+}
+
+
+function productSortLess() {
+
+    showProducts(productList.sort(sortByPriceAsc));
+    
+    
+    }
+//Para el onclick//
+  
+
+
+function showProducts(list){
+    let htmlContentToAppend = "";
+for(let i=0; i < list.length;i++){
+let product = list[i]
+htmlContentToAppend += ` 
+  
+ <div class="list-group-item-action list-group-item-action" >
+ <div class="row">
+  <div class="col-3>
+    <img src = " ${product.image} " alt= "product image" class="img-thumbnail">
+    </div>
+      <div class="col">
+         <div class="d-flex w-100 justify-content-between">
+           <div class="mb-1">
+              <h4> ${product.name} -   ${product.currency}  ${product.cost }   </h4>
+              <p> ${product.description}  </p>
+              <br> <br>
+                
+                 </div>
+
+                 <small class="text-muted"> ${product.soldCount }  vendidos </small>
+
+                         </div>
+
+
+                       </div>
+
+
+                    </div>
+
+
+                 </div>
+
+ 
+ `
+
+    }
+    document.getElementById("info").innerHTML = htmlContentToAppend;
+     catText()
+}
+
+//Definition//
+function catText(){
+    document.getElementById("catName").innerText = catNames[localStorage.getItem('catID')]
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_API).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            productList = resultObj.data.products;
+            console.log(productList)
+            showProducts(productList);
+        }
+    });
+
+});
+//DEFSEL//
+
+
+
+
+//33//
