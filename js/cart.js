e1b404cb44901a1558003af204432b5f66dea313
lucascardozo <109979488+Lucas20034@users.noreply.github.com>
2022-10-12 16:09:56 -0300

    Correcion de los mistakes

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,6 @@
-const PAPEL = ('https://japceibal.github.io/emercado-api/user_cart/25801.json')
+const PAPEL = ('https://japceibal.github.io/emercado-api/user_cart/25801.json') // Llamo a la API que necesito// 
+
+//peticion feth 
 
 
 fetch(PAPEL)
@@ -20,16 +22,25 @@ function ShowProduct(array) {
             <div class="col-3">
                 <img src="${propent.image}"  class="img-thumbnail" style=" height:10em; border-radius: 60px; border-style: solid; border-width: 5px; border-color: #c5c5c556; ">
             </div>
+            
             <div class="col">
-                <div class="d-flex w-100 justify-content-between">
-                    <h2 class="mb-1">${propent.name}</h2>
-                     <h4 class="mb-1">${propent.count}</h4>
-                    <h4 class="mb-1">${propent.unitCost} ${propent.currency}</h4>
+               <div class="d-flex w-100 justify-content-between"> 
+                    <h2 class="mb-1">   <p> Nombre <hr> </hr> <p>${propent.name}</h2>
+
+                    <h2 class="mb-1"> <p> Costo <hr> </hr>  <p>  ${propent.unitCost} </h2>
+
+                     <h3 class="mb-1"> <p> Cantidad <hr> </hr> <p>  <input type="number" style=" height:3em;  width:3em;">  </h3>
+
+
+                    <h4 class="mb-1"> <p> Subtotal <hr> </hr>  <p> ${propent.unitCost} ${propent.currency}</h4>
+                    <hr> </hr>  
                     
                 </div>
                 
             </div>
+            
         </div>
+        
     </div>
     `
 
@@ -37,3 +48,13 @@ function ShowProduct(array) {
     });
 
 }
+
+const Cifra= 15200
+
+function Calculator()  {
+//Count//
+Cifra *  unitCost.value
+console.log(Calculator)
+
+}
+Calculator()
